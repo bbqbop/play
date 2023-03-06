431fcb89cc19558940a28deb2808a67153a18520
bbqbop <dierk.peters@gmail.com>
2023-03-06 14:03:29 -0500

    add grid select & set absolute size on container

---
+++ etch_a_sketch/script.js
@@ -0,0 +1,34 @@
+function clearBox(element)
+{
+    element.innerHTML = "";
+}
+
+const container = document.querySelector('.container');
+let gridSize = 16;
+drawGrid();
+
+const inpGridSize = document.querySelector('#gridSize');
+inpGridSize.addEventListener('change', (e) => {
+    gridSize = e.target.value;
+    drawGrid();
+})
+
+
+function drawGrid(){
+    clearBox(container);
+    for (i = 1; i <= gridSize; i++){
+        const gridRow = document.createElement('div')
+        gridRow.classList.add('gridRow');
+        container.appendChild(gridRow);
+        for (j = 1; j <= gridSize; j++){
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('gridItem');
+            gridRow.appendChild(gridItem);
+            gridItem.addEventListener ('mouseover', (event) => {
+                event.target.style.backgroundColor = "black";
+                
+            }
+            )
+        }
+    }
+}
