af1800a278751d09e349ad3c072b705725d3260e
bbqbop <dierk.peters@gmail.com>
2023-03-05 23:56:30 -0500

    create an UI for the RPS Game

--- GAME_rock-paper-scissor/script.js
+++ GAME_rock-paper-scissor/script.js
@@ -26,18 +26,34 @@ function capitalize(str) {
     return str[0].toUpperCase() + str.substring(1);
 }
 
-function playRound(userChoice, computerChoice){
+let winCounter = 0;
+let loseCounter = 0;
+let drawCounter = 0;
+
+function playRound(userChoice){
+    let computerChoice = getComputerChoice()
     if (userChoice === computerChoice){ 
-        return `DRAW! ${capitalize(rPs[userChoice])} vs ${capitalize(rPs[computerChoice])}`;
+        result.textContent = `DRAW! ${capitalize(rPs[userChoice])} vs ${capitalize(rPs[computerChoice])}`;
+        drawCounter += 1;
+        
         
     } else if (
         userChoice == 0 && computerChoice == 2 || 
         userChoice == 1 && computerChoice == 0 ||
         userChoice == 2 && computerChoice == 1 ){
-        return `YOU WIN! ${capitalize(rPs[userChoice])} beats ${capitalize(rPs[computerChoice])}`;
+        result.textContent = `YOU WIN! ${capitalize(rPs[userChoice])} beats ${capitalize(rPs[computerChoice])}`;
+        winCounter += 1;
    } else { 
-        return `YOU LOSE! ${capitalize(rPs[computerChoice])} beats ${capitalize(rPs[userChoice])}`
-    }}
+        result.textContent = `YOU LOSE! ${capitalize(rPs[computerChoice])} beats ${capitalize(rPs[userChoice])}`;
+        loseCounter += 1;
+    }
+    if (winCounter === 5 || loseCounter === 5){
+        gameOver()
+        
+    }
+    counter.textContent = `WINS: ${winCounter} | LOSES: ${loseCounter} | DRAWS: ${drawCounter}`;
+
+}
 
 function game() {
     let draw = 0
@@ -59,3 +75,37 @@ function game() {
     let result = win > lose ? 'YOU WON!' : 'YOU LOST!';
     console.log(`${result} ${win} wins, ${lose} defeats, ${draw} draws`)
 }
+
+const buttons = document.querySelectorAll('button');
+  const rock = document.querySelector('#rock');
+  rock.addEventListener('click', () => playRound(0))
+
+  const paper = document.querySelector('#paper');
+  paper.addEventListener('click', () => playRound(1))
+
+  const scissors = document.querySelector('#scissors');
+  scissors.addEventListener('click', () => playRound(2))
+
+const results = document.querySelector('.results')
+const result = document.querySelector('#result');
+const counter = document.querySelector('#counter');
+
+function gameOver (){
+    buttons.forEach(button => button.remove())
+    result.classList.add('gameover');
+    if (winCounter > loseCounter
+        ? result.textContent = `YOU WON` 
+        : result.textContent = `YOU LOSE`);
+    const restartBtn = document.createElement('button', id='restart');
+    restartBtn.textContent = 'PLAY AGAIN';
+    results.appendChild(restartBtn);  
+    
+    ;
+}
+
+
+
+
+
+
+
