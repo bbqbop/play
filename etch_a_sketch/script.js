b709ec5d89e3aadc7995706007827f629131ba59
bbqbop <dierk.peters@gmail.com>
2023-03-06 23:21:33 -0500

    Linked all color selectors, trouble apply to Items

--- etch_a_sketch/script.js
+++ etch_a_sketch/script.js
@@ -1,37 +1,49 @@
-function clearBox(element)
-{
-    element.innerHTML = "";
-}
-
-const container = document.querySelector('.container');
+let gridRow;
+let gridItem;
 let gridSize = 16;
-drawGrid();
-
-const btnClear = document.querySelector('#btnClear');
-btnClear.addEventListener('click', () => drawGrid())
+let selectAll;
 
 const inpGridSize = document.querySelector('#gridSize');
+const btnClear = document.querySelector('#btnClear');
+const chooseColor = document.querySelector('#chooseColor');
+const container = document.querySelector('.container');
 
 inpGridSize.addEventListener('change', (e) => {
     gridSize = e.target.value;
     drawGrid();
 })
 
+btnClear.addEventListener('click', () => drawGrid())
+function clearBox(element)
+{
+    element.innerHTML = "";
+}
+
+chooseColor.addEventListener('change', (e) => {
+    color = e.target.value;
+    changeColor(color);
+})
+
+function changeColor(color){
+    gridItem.addEventListener ('mouseover', (event) => {
+    event.target.style.backgroundColor = color;
+        })
+    }
+
 function drawGrid(){
     clearBox(container);
     for (i = 1; i <= gridSize; i++){
-        const gridRow = document.createElement('div')
+        gridRow = document.createElement('div')
         gridRow.classList.add('gridRow');
         container.appendChild(gridRow);
         for (j = 1; j <= gridSize; j++){
-            const gridItem = document.createElement('div');
+            gridItem = document.createElement('div');
             gridItem.classList.add('gridItem');
             gridRow.appendChild(gridItem);
-            gridItem.addEventListener ('mouseover', (event) => {
-                event.target.style.backgroundColor = "black";
-                
-            }
-            )
+            changeColor('black');
         }
     }
 }
+
+
+drawGrid();
